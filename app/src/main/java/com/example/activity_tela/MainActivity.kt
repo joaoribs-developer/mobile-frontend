8b7f8b2fa84d388ea8edae0774cab80c2036a119
João Ribeiro <joaoribeiroh1@gmail.com>
2022-06-22 11:10:49 -0300

    tela login pay

---
+++ app/src/main/java/com/example/activity_tela/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.activity_tela
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity :Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_telalogin)
+    }
+}
